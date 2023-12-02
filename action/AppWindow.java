2d90acbedc63e700bc02376e51f27ad866e7fa0c
sworc <lightbox@nifty.com>
2023-12-02 16:55:32 +0900

    Add files via upload

--- action/AppWindow.java
+++ action/AppWindow.java
@@ -1,20 +1,42 @@
 package action;
 
+import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 
 public class AppWindow extends BaseWindow {
 
+    private Connection conn = null;
+    private Statement stmt = null;
+    private ResultSet rs = null;
+
     private JPanel jContentPane = null;
     private JButton jButton = null;
-    public JTextField jText = null;
-    public JTextField jText2 = null;
-    public JTextField jText3 = null;
+
+    // 社員コード
+    private JLabel lblScode = null;
+    private JTextField jScode = null;
+
+    // 氏名
+    private JLabel lblSname = null;
+    private JTextField jSname = null;
+
+    // フリガナ
+    private JLabel lblFuri = null;
+    private JTextField jFuri = null;
+
+    // 性別
+    private JLabel lblSeibetu = null;
+    private JTextField jSeibetu = null;
+
+    private JComboBox<String> genderComboBox;
+    private Object[] genders = {0, 1};
+    private String[] genderLabels = {"男性", "女性"};
 
     public int mainWidth = 600;
     public int mainHeight = 400;
-    public String titleString = "Swing 簡易入力サンプル( GridLayout )";
+    public String titleString = "Swing 簡易サンプル";
 
     // *****************************************************
     // ボタン作成とクリックイベント
@@ -22,21 +44,81 @@ public class AppWindow extends BaseWindow {
     private JButton getJButton() {
         if (jButton == null) {
             jButton = new JButton();
+            // メインウインドウに対して、100x30 のボタンを追加
+            jButton.setBounds( 250, 30-7, 100, 30 );
             jButton.setText("実行");
             jButton.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent e) {
 
-                    AppWindow.this.MsgOk("ボタンがクリックされました");
-                    System.out.println("ボタンがクリックされました");
-
                     try {
-                        // ボタン処理用クラスの呼び出し
-                        Action.testProc(AppWindow.this);
+                        conn = DriverManager.getConnection(
+                            "jdbc:mysql://localhost/lightbox?user=root&password=&characterEncoding=UTF-8"
+                        );
+
+                        // ステートメント
+                        stmt = conn.createStatement();
+                        // SQL 実行
+                        String sCode = jScode.getText();
+                        rs = stmt.executeQuery("select * from 社員マスタ where 社員コード = '" + sCode + "'");
+
+                        // 一行取り出し
+                        rs.next();
+
+                        // 社員名
+                        String sName = rs.getString("氏名");
+                        jSname.setText(sName);
+
+                        // 社員名
+                        String sFuri = rs.getString("フリガナ");
+                        jFuri.setText(sFuri);
+
+
+                        // 整数
+                        int seibetu = rs.getInt("性別");
+                        jSeibetu.setText(seibetu+"");
+                        genderComboBox.setSelectedIndex(seibetu);
+
+
+                        // 文字列
+                        String seibetu2 = String.format("%d", seibetu);
+                        if ( seibetu2.equals("0") ) {
+                            System.out.println("男性");
+                        }
+                        else {
+                            System.out.println("女性");
+                        }
+
+                        // while( rs.next() ) {
+
+                        //     System.out.println(rs.getString("社員コード"));
+                        //     System.out.println(rs.getString("氏名"));
+                        //     System.out.println(rs.getString("フリガナ"));
+                        //     System.out.println(rs.getString("所属"));
+                        //     System.out.println(rs.getString("性別"));
+                        //     System.out.println(rs.getString("作成日"));
+                        //     System.out.println(rs.getString("更新日"));
+                        //     System.out.println(rs.getString("給与"));
+                        //     System.out.println(rs.getString("手当"));
+                        //     System.out.println(rs.getString("管理者"));
+                        //     System.out.println(rs.getString("生年月日"));
+
+                        // }
+
+                        rs.close();
+                        stmt.close();
+                        conn.close();
 
                     }
-                    catch ( Exception ex ) {
+                    catch (Exception ex) {
+                        System.out.println( ex.getMessage() );
                         ex.printStackTrace();
                     }
+
+                    // 編集状態の切り替え
+                    jScode.setEditable(false);
+                    jSname.setEditable(true);
+                    jFuri.setEditable(true);
+                    genderComboBox.setEnabled(true);
 
                 }
             });
@@ -79,25 +161,54 @@ public class AppWindow extends BaseWindow {
     private JPanel getJContentPane() {
         if (jContentPane == null) {
             jContentPane = new JPanel();
-            jContentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-            GridLayout layout = new GridLayout(10,1);
-            layout.setVgap(10);  //垂直方向の間隔
-            jContentPane.setLayout(layout);
+            jContentPane.setLayout(null);
+            jContentPane.add(getJButton(), null);
 
-            // 入力フィールド
-            jText = new JTextField();
-            jText.setText("1234567890");
-            jContentPane.add(jText);
-            // 入力フィールド2
-            jText2 = new JTextField();
-            jText2.setText("ABCDEFG");
-            jContentPane.add(jText2);
-            // 入力フィールド3
-            jText3 = new JTextField();
-            jText3.setText("日本語表示");
-            jContentPane.add(jText3);
+            lblScode = new JLabel("社員コード");
+            lblScode.setBounds(60, 30, 80, 19);
+            jContentPane.add(lblScode);
 
-            jContentPane.add(getJButton());
+            jScode = new JTextField();
+            jScode.setBounds(150, 30, 60, 19);
+            jContentPane.add(jScode);
+
+            lblSname = new JLabel("氏名");
+            lblSname.setBounds(60, 30+50, 80, 19);
+            jContentPane.add(lblSname);
+
+            jSname = new JTextField();
+            jSname.setBounds(150, 30+50, 120, 19);
+            jContentPane.add(jSname);
+
+            lblFuri = new JLabel("フリガナ");
+            lblFuri.setBounds(60, 30+50+30, 80, 19);
+            jContentPane.add(lblFuri);
+
+            jFuri = new JTextField();
+            jFuri.setBounds(150, 30+50+30, 120, 19);
+            jContentPane.add(jFuri);
+
+            lblSeibetu = new JLabel("性別");
+            lblSeibetu.setBounds(60, 30+50+30+30, 80, 19);
+            jContentPane.add(lblSeibetu);
+
+            // 非表示の性別テキストフィールド
+            jSeibetu = new JTextField();
+            jSeibetu.setBounds(150, 30+50+30+30, 30, 19);
+            jContentPane.add(jSeibetu);
+            jSeibetu.setVisible(false);
+
+            genderComboBox = new JComboBox<>(genderLabels);
+            genderComboBox.setBounds(150, 30+50+30+30+30, 150, 19);
+            jContentPane.add(genderComboBox);
+
+
+            // 初期状態の画面制御
+            // jSname.setEnabled(false);
+            jSname.setEditable(false);
+            jFuri.setEditable(false);
+            genderComboBox.setEnabled(false);
+
         }
         return jContentPane;
     }
